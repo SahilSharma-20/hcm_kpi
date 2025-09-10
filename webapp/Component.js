4b5f69c28c856656248976ec56c1f7ea739e8531
Sahil Kumar <sahilpusa@gmail.com>
2025-09-10 05:49:16 +0000

    Initial commit of SAP UI5 HCM KPI Prototype  application

---
+++ webapp/Component.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "hcm/hcmkpi/model/models"
+], (UIComponent, models) => {
+    "use strict";
+
+    return UIComponent.extend("hcm.hcmkpi.Component", {
+        metadata: {
+            manifest: "json",
+            interfaces: [
+                "sap.ui.core.IAsyncContentCreation"
+            ]
+        },
+
+        init() {
+            UIComponent.prototype.init.apply(this, arguments);
+
+            this.setModel(models.createDeviceModel(), "device");
+
+            this.getRouter().initialize();
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"hcm/hcmkpi/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
